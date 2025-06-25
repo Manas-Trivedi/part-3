bfbf618987691260a673fba7257d2f97aa9b5db1
Manas Trivedi <manas.trivedi.020@gmail.com>
2025-06-26 01:33:20 +0530

    All exercises in part 3-1 (3.1 to 3.6)

---
+++ phonebook/index.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const fs = require('fs')
+let contacts = require('./contacts.json')
+const app = express()
+
+app.use(express.json())
+
+app.get('/api/persons', (req, res) => {
+    res.json(contacts);
+})
+
+app.get('/api/info', (req, res) => {
+    let allContacts = `Phonebook currently has ${contacts.length} contacts.`
+    message = `${allContacts}<br>${new Date()}`
+    res.send(message);
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = req.params.id
+    const person = contacts.find(p => p.id === id)
+    if(person)
+        res.json(person)
+    else
+        res.status(404).end()
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = req.params.id
+    contacts = contacts.filter(p => p.id != id)
+    fs.writeFileSync('./contacts.json', JSON.stringify(contacts, null, 4))
+    res.status(204).end()
+})
+
+app.post('/api/persons', (req, res) => {
+    const body = req.body
+    if(!body) {
+        return res.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    const name = body.name
+    if(contacts.find(p => p.name === name))
+        return res.status(400).json({error: 'Name already exists'})
+    const person = {
+        id: Math.ceil(Math.random() * (1000 - 4) + 4),
+        name: body.name,
+        number: body.number
+    }
+    contacts = contacts.concat(person)
+    fs.writeFileSync('./contacts.json', JSON.stringify(contacts, null, 4))
+    res.json(person)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on Port ${PORT}`)
+})
